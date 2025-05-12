254fd6adc145fb22006e1beaac6b277139fa0118
aarsh <wh0am1.7548@gmail.com>
2025-05-11 17:18:20 -0700

    add operations

--- api/dms/serializers/__init__.py
+++ api/dms/serializers/__init__.py
@@ -2,6 +2,7 @@ from .common_serializers import *
 from .driver_serializers import *
 from .plan_validation import *
 from .profile_serializers import ProfileSerializer
+from .operation_serializers import *
 from .order_serializers import *
 from .trip_serializers import *
 from .vehicle_serializers import *
